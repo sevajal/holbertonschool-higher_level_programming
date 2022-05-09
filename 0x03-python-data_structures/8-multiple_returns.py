a0301176ed7d61159938798c99cc4ea71acf357c
sevajal <sevajal@yahoo.com>
2022-05-09 12:48:36 -0500

    I forgot the none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        first = 0
+        first = None
     else:
         first = sentence[0]
     tuple = (length, first)
