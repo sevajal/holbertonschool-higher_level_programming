7115aa21321445b01308f2f48a61a562369da0e7
sevajal <sevajal@yahoo.com>
2022-05-10 13:15:15 -0500

    I included the condition to 4

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,26 +6,35 @@ def roman_to_int(roman_string):
         i = 0
         while i < length:
             if roman_string[i] == "M":
-                if roman_string[i - 1] == "C":
+                if i != 0 and roman_string[i - 1] == "C":
                     result += 800
                 else:
                     result += 1000
             elif roman_string[i] == "D":
-                result += 500
+                if i != 0 and roman_string[i - 1] == "X":
+                    result += 300
+                else:
+                    result += 500
             elif roman_string[i] == "C":
-                if roman_string[i - 1] == "X":
+                if i != 0 and roman_string[i - 1] == "X":
                     result += 80
                 else:
                     result += 100
             elif roman_string[i] == "L":
-                result += 50
+                if i != 0 and roman_string[i - 1] == "X":
+                    result += 30
+                else:
+                    result += 50
             elif roman_string[i] == "X":
-                if roman_string[i - 1] == "I":
+                if i != 0 and roman_string[i - 1] == "I":
                     result += 8
                 else:
                     result += 10
             elif roman_string[i] == "V":
-                result += 5
+                if i != 0 and roman_string[i - 1] == "I":
+                    result += 3
+                else:
+                    result += 5
             elif roman_string[i] == "I":
                 result += 1
             i += 1
