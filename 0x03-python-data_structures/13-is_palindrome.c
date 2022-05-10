2020a2b18865317618a30a3ad0af4a802ee45a05
sevajal <sevajal@yahoo.com>
2022-05-09 22:03:02 -0500

    New aproach

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,14 +8,14 @@
 
 int is_palindrome(listint_t **head)
 {
-	unsigned int i, nodes, half;
+	unsigned int i = 0, nodes = 0, half;
 	const listint_t *forward = *head;
-	const listint_t *back = *head;
+	const listint_t *back;
+	const listint_t *half_head = *head;
 
 	if (forward  == NULL)
 		return (1);
 
-	nodes = 0;
 	while (forward != NULL)
 	{
 		forward = forward->next;
@@ -23,6 +23,11 @@ int is_palindrome(listint_t **head)
 	}
 	nodes--;
 	half = nodes / 2;
+	while (i <= half)
+	{
+		half_head = half_head->next;
+		i++;
+	}
 	forward = *head;
 
 	if (nodes == 1)
@@ -30,15 +35,16 @@ int is_palindrome(listint_t **head)
 
 	if (nodes > 1)
 	{
-		while (nodes > half)
+		back = half_head;
+		while (half > 0)
 		{
-			for (i = 0; i < nodes; i++)
+			for (i = 0; i < half; i++)
 				back = back->next;
 			if (forward->n != back->n)
 				return (0);
 			forward = forward->next;
-			back = *head;
-			nodes--;
+			back = half_head;
+			half--;
 		}
 	}
 	return (1);
