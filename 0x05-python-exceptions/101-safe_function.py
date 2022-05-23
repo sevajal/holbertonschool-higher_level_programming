f305a03e948973b457739d51702e41b6e8b8c904
sevajal <sevajal@yahoo.com>
2022-05-23 15:16:25 -0500

    I included the return

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,6 +2,7 @@
 def safe_function(fct, *args):
     import sys
     try:
-        fct(*args)
-    except BaseException as error:
+        return (fct(*args))
+    except Exception as error:
         print("Exception:", error, file=sys.stderr)
+        
