16e01564c786a0993f99ffc96795b4acc05de862
sevajal <sevajal@yahoo.com>
2022-05-23 11:27:08 -0500

    I did the task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return (a / b)
+    except ZeroDivisionError:
+        result = None
+        return (None)
+    finally:
+        print("Inside result: {}".format(result))
