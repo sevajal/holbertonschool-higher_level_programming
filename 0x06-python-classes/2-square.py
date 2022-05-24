946640cbf323295152a08b8e689de05675adfcb1
sevajal <sevajal@yahoo.com>
2022-05-24 16:07:15 -0500

    I added documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
 class Square:
+    """class Square that defines a square"""
+
     def __init__(self, size=0):
+        """__init__ constructor method: creates a square object.
+        args: size - size of the square.
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
