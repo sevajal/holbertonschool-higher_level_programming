943c4086455b37add1fc3931d754c77752c4a18b
sevajal <sevajal@yahoo.com>
2022-05-24 16:09:35 -0500

    I added documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
 class Square:
+    """class Square that defines a square"""
+
     def __init__(self, size=0):
+        """__init__ constructor method: creates a square object.
+        args: size - size of the square.
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
+        """area method: return the square area"""
         return (self.__size * self.__size)
