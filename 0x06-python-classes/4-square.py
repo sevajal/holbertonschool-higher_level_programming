2b3e998cf476a90510c76c81fe2933f6c0f8db20
sevajal <sevajal@yahoo.com>
2022-05-24 18:37:47 -0500

    I am checking private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,7 +13,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = size
 
     def area(self):
         """Return the square area"""
