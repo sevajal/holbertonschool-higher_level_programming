fc70f0cf092b8bafa23a488c748f11a11ec5348e
sevajal <sevajal@yahoo.com>
2022-05-24 18:12:09 -0500

    first draft task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """__init__ constructor method: creates a square object.
+        args: size - size of the square."""
+        self.size = size
+
+    def area(self):
+        """Return the square area"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Retrieve the square size"""
+        return (self.__size)
+
+    @property
+    def size(self, value):
+        """Set the square size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
