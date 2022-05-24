5ca316dfc1719283f6557c3ad6241bc371bcf1d1
sevajal <sevajal@yahoo.com>
2022-05-24 16:05:24 -0500

    I added documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
 class Square:
+    """class Square that defines a square"""
+
     def __init__(self, size):
+        """__init__ constructor method: creates a square object.
+        args: size - size of the square.
+        """
         self.__size = size
