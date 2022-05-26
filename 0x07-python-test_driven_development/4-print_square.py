f6fa3644f0eaa6d3a4fe0b39fa23e76b259e596c
sevajal <sevajal@yahoo.com>
2022-05-26 17:14:52 -0500

    I did the task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""4-print_square module that prints a square with the character #."""
+
+
+def print_square(size):
+    """Function that prints a square with the character #."""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    [print("#" * size) for i in range(size)]
