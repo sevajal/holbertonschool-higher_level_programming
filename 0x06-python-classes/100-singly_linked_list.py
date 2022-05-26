a31cab42776632901d7162ac832ec552ffa0b24b
sevajal <sevajal@yahoo.com>
2022-05-25 22:26:10 -0500

    I did the task 100

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""Program to Write a class Node that defines a node of a singly linked list"""
+
+
+class Node:
+	"""Creates a class Node"""
+	def __init__(self, data, next_node=None):
+		"""Constructor method __init__"""
+		self.data = data
+		self.next_node = next_node
+
+	@property
+	def data(self):
+		"""Getter method for data"""
+		return (self.__data)
+
+	@data.setter
+	def data(self, value):
+		"""Setter method for data"""
+		if type(value) != int:
+			raise TypeError("data must be an integer")
+		self.__data = value
+
+	@property
+	def next_node(self):
+		"""Getter method for next_node"""
+		return (self.__next_node)
+
+	@next_node.setter
+	def next_node(self, value):
+		"""Setter method for next_node"""
+		if Node or value == None:
+			self.__next_node = value
+		else:
+			raise TypeError("next_node must be a Node object")
+
+class SinglyLinkedList:
+	"""Creates a class SinglyLinkedList"""
+	def __init__(self):
+		"""Constructor method __init__"""
+		self.__head = None
+
+	def __str__(self):
+		"""print method for printing the sll"""
+		h = self.__head
+		node = ""
+		while (h):
+			node += str(h.data)
+			h = h.next_node
+			if (h):
+				node += "\n"
+		return (node)
+
+	def sorted_insert(self, value):
+		"""Method for insert a Node in sorted order"""
+		h = self.__head
+		while (h):
+			if h.data > value:
+				break
+			tmp = h
+			h = h.next_node
+		new = Node(value, h)
+		if h == self.__head:
+			self.__head = new
+		else:
+			tmp.next_node = new
