eba13595998bacccf624f56bbdd0f0f2de4539f2
sevajal <sevajal@yahoo.com>
2022-05-25 20:56:15 -0500

    I imported math, included type and change circumference formula

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,19 +2,21 @@
 
 """MagicClass program"""
 
+import math
+
 
 class MagicClass:
     """Create a class called MagicClass"""
     def __init__(self, radius):
         """Constructor method __init___"""
-        if radius is not int or radius is not float:
+        if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
         """Return the area of a circle"""
-        return ((self.__radius ** 2) * math(pi))
+        return ((self.__radius ** 2) * math.pi)
 
     def circumference(self):
         """Return the circumference of a circle"""
-        return (math(pi) * (self.__radius ** 2))
+        return (2 * math.pi * self.__radius)
