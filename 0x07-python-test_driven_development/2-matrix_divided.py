c5550b6d9f059da973cfd555e20a08c04c57fb65
sevajal <sevajal@yahoo.com>
2022-05-28 12:25:24 -0500

    I changed the code to consider matrix bigger than 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,29 +5,24 @@
 
 def matrix_divided(matrix, div):
     """Function that divides all elements of a matrix"""
-    if type(matrix) is not list or type(matrix[0]) is not list \
-            or type(matrix[1]) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) \
-            of integers/floats")
-    for x in matrix[0]:
-        if type(x) not in [int, float]:
-            raise TypeError("matrix must be a matrix (list of lists) \
-                of integers/floats")
-    for x in matrix[1]:
-        if type(x) not in [int, float]:
-            raise TypeError("matrix must be a matrix (list of lists) \
-                of integers/floats")
-    if len(matrix[0]) is not len(matrix[1]):
-        raise TypeError("Each row of the matrix must have the same size")
+    e = "matrix must be a matrix (list of lists) of integers/floats"
+    result = []
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    result = []
-    for x in range(2):
-        result.append([])
-    for x in range(len(matrix[0])):
-        result[0].append(round(float(matrix[0][x] / div), 2))
-    for x in range(len(matrix[1])):
-        result[1].append(round(float(matrix[1][x] / div), 2))
+    if type(matrix) is not list:
+        raise TypeError(e)
+    if len(matrix) == 0:
+        raise TypeError(e)
+    if not all([len(x) == len(matrix[0]) for x in matrix]):
+        raise TypeError("Each row of the matrix must have the same size")
+    for x in matrix:
+        if type(x) is not list:
+            raise TypeError(e)
+        for element in x:
+            if type(element) not in [int, float]:
+                raise TypeError(e)
+        row = [round(element / div, 2) for element in x]
+        result.append(row)
     return (result)
