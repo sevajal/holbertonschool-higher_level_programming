817957c1fde2af200fabc1eb8b880b0227e1188f
sevajal <sevajal@yahoo.com>
2022-05-31 21:14:59 -0500

    I added if a and b

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,9 @@
 
 def add_integer(a, b=98):
     """function that returns the add of two integers"""
-    if type(a) not in [int, float]:
+    if not a or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not b or type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
