41893d01c2ec077a1bdfd2c4c8b1c0dfc92a518d
sevajal <sevajal@yahoo.com>
2022-05-31 21:33:45 -0500

    I added test for empty function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,7 +5,7 @@
 
 def say_my_name(first_name, last_name=""):
     """Function that prints My name is <first name> <last name>"""
-    if not first_name or type(first_name) is not str:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
