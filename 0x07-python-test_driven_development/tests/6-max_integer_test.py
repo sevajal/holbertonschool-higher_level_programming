934f5c0c43c96fb88d1881163ebb9cb84762b032
sevajal <sevajal@yahoo.com>
2022-05-31 21:51:15 -0500

    I added test for max in middle

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,7 +12,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1]), 1)
         self.assertEqual(max_integer([4, -8, 0, 1]), 4)
-        self.assertEqual(max_integer([-10, -8, -6, -4]), -4)
+        self.assertEqual(max_integer([-10, -8, -2, -4]), -2)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([4, -8, 0, 1.54]), 4)
         self.assertEqual(max_integer("holaz"), "z")
