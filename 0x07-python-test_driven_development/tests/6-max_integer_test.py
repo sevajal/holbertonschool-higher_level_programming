be9ea136f5c9e85c29785343415c4a4c6494fd34
sevajal <sevajal@yahoo.com>
2022-05-31 21:49:02 -0500

    I added test for only negative

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,6 +12,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1]), 1)
         self.assertEqual(max_integer([4, -8, 0, 1]), 4)
+        self.assertEqual(max_integer([-10, -8, -6, -4]), -4)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([4, -8, 0, 1.54]), 4)
         self.assertEqual(max_integer("holaz"), "z")
@@ -26,4 +27,4 @@ class TestMaxInteger(unittest.TestCase):
         self.assertRaises(TypeError, max_integer, [1, 3, "5"])
         self.assertRaises(TypeError, max_integer, [1, 100, "h"])
         self.assertRaises(TypeError, max_integer, [1, 3, [5, 7]])
-        self.assertRaises(TypeError, max_integer, [(5, 7)])
+        self.assertRaises(TypeError, max_integer, [1, 3, (5, 7)])
