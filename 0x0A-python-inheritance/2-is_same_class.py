800fb15ab508e4ccbff1f691c9ae404ab53d6fd7
sevajal <sevajal@yahoo.com>
2022-06-07 12:44:37 -0500

    I changed the code to only true for int

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@
 def is_same_class(obj, a_class):
     """Returns True if the object is exactly an instance of the specified class
     otherwise False."""
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
