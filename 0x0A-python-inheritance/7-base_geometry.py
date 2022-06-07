9727129c0d23780e8d646979c524d57ec801922c
sevajal <sevajal@yahoo.com>
2022-06-07 18:31:12 -0500

    Back with the setting

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,3 +16,4 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+        self.value = value
