6ab074d967ce1706de0f006ef12343b0730ee154
sevajal <sevajal@yahoo.com>
2022-06-07 18:20:20 -0500

    I fixed the TypeError

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Integer_validator method"""
         if type(value) is not int:
-            raise TypeError(f"{name} must be a int")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
