7199dd436fb53170dc6c7fd07bf0b38bd1f6c107
sevajal <sevajal@yahoo.com>
2022-06-07 18:51:04 -0500

    I changed the __init__

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
         """__init__ constructor method"""
-        self.integer_validator("width", width)
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
