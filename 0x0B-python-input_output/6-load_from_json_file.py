21264a8d67fb2469bfcb075774905a24f973af91
sevajal <sevajal@yahoo.com>
2022-06-08 15:19:04 -0500

    First draft task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""load_from_json_file function"""
+
+
+def load_from_json_file(filename):
+    """Function that creates an Object from a “JSON file"""
+    import json
+    with open(filename, encoding="utf-8") as file:
+        return json.load(file)
