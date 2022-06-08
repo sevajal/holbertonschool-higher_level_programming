68057a991daaba92d586a6c36e23ec29ba36a5e7
sevajal <sevajal@yahoo.com>
2022-06-07 21:08:57 -0500

    I imported the Rectangle class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,54 +1,9 @@
 #!/usr/bin/python3
 
-"""BaseGeometry class"""
-
-
-class BaseGeometry:
-    """BaseGeometry class"""
-
-    def area(self):
-        """Area method"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Integer_validator method"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-"""BaseGeometry class"""
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
-    def __init__(self, width, height):
-        """__init__ constructor method"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """Area method"""
-        return self.__width * self.__height
-
-    def print(self):
-        """Print method"""
-        width = self.__width
-        height = self.__height
-        print(f"[Rectangle] {width}/{height}")
-
-    def __str__(self):
-        """__str__ method"""
-        width = str(self.__width)
-        height = str(self.__height)
-        result = "[Rectangle] " + width + "/" + height
-        return result
-
-
 """Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -63,11 +18,6 @@ class Square(Rectangle):
     def area(self):
         """Area method"""
         return self.__size ** 2
-
-    def print(self):
-        """Print method"""
-        size = self.__size
-        print(f"[Square] {size}/{size}")
 
     def __str__(self):
         """__str__ method"""
