0f68c6ada840fee5cd80a455378a1eadd78799b4
sevajal <sevajal@yahoo.com>
2022-06-08 16:36:22 -0500

    Pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list, and then save them to a file"""
+"""script that adds all arguments to a Python list,
+and then save them to a file"""
 import sys
 import os.path
 save = __import__('5-save_to_json_file').save_to_json_file
