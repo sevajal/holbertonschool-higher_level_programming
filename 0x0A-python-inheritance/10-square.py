a8f1ca8ba957ab4f5822e0021cbe8a309bb5dbad
sevajal <sevajal@yahoo.com>
2022-06-07 21:07:22 -0500

    I imported the Rectangle class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,33 +1,9 @@
 #!/usr/bin/python3
 
-"""Rectangle class"""
-
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
-    def __init__(self, width, height):
-        """__init__ constructor method"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """Area method"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """__str__ method"""
-        width = str(self.__width)
-        height = str(self.__height)
-        result = "[Rectangle] " + width + "/" + height
-        return result
-
-
 """Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
