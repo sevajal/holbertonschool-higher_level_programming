92a8f6ecd23bfc463a81d21c340c1a1a765cb748
sevajal <sevajal@yahoo.com>
2022-06-10 14:37:46 -0500

    I did the task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -62,3 +62,7 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Method that returns the dictionary representation of a Rectangle"""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
