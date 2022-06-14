96561502540bae8a7b63ab8d24881f508f90c27b
sevajal <sevajal@yahoo.com>
2022-06-14 13:39:58 -0500

    back

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,11 +33,13 @@ class Base:
     def save_to_file(cls, list_objs):
         """Method that writes the JSON string representation of
         list_objs to a file"""
+        my_list = []
         if list_objs is None:
-            return "[]"
+            return my_list
         filename = cls.__name__ + ".json"
-        with open(filename, mode="w", encoding="utf-8") as f:
-            f.write(cls.to_json_string([x.to_dictionary() for x in list_objs]))
+        my_list = [item.to_dictionary() for item in list_objs]
+        with open(filename, mode="w", encoding="utf-8") as file:
+            file.write(cls.to_json_string(my_list))
 
     @staticmethod
     def from_json_string(json_string):
