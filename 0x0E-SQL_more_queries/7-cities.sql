61675326e9fbc399605b321c8c8570c099432497
sevajal <sevajal@yahoo.com>
2022-07-13 17:38:27 -0500

    I did the task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on my MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT, 
+    state_id INT NOT NULL, FOREIGN KEY(states_id) REFERENCES states(id), 
+    name VARCHAR(256) NOT NULL
+    ) ENGINE=INNODB;
