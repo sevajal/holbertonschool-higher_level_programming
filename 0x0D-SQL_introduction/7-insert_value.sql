57889f5ddcf2b86eed1cb905e9c6d0421346da30
sevajal <sevajal@yahoo.com>
2022-07-12 20:40:24 -0500

    I did the task 8 and changed 98 for 89 in task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-INSERT INTO `first_table` VALUES (98, "Best School");
+INSERT INTO `first_table` VALUES (89, "Best School");
