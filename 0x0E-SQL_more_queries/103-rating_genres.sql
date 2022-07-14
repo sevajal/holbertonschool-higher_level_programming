f5f893e7e13205c3eea055ce128c34bc89d99617
sevajal <sevajal@yahoo.com>
2022-07-14 15:48:29 -0500

    I did the task 103

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- Script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
