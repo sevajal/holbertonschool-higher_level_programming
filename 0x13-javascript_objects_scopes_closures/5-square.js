8d7403e7ddd16382fa19b7b243cc9fbb46d83918
sevajal <sevajal@yahoo.com>
2022-08-16 12:30:51 -0500

    I changed the super to size, size

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,8 +4,6 @@ const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
-    super();
-    this.width = size;
-    this.height = size;
+    super(size, size);
   }
 };
