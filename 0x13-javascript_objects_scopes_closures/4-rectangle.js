527d6d4c5d040d79f6443f237974afc7d2a09367
sevajal <sevajal@yahoo.com>
2022-08-16 11:59:19 -0500

    I did the task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const char = 'X';
+    let string = '';
+    for (let i = 0; i < this.width; i++) {
+      string += char;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(string);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
