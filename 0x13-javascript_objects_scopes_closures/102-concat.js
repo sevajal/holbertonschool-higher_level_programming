f6aa35dfce7965506585b1b8d7844db3fa9171f1
sevajal <sevajal@yahoo.com>
2022-08-16 17:47:53 -0500

    I did the task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv;
+const fs = require('fs');
+const data1 = fs.readFileSync(args[2], 'utf8');
+const data2 = fs.readFileSync(args[3], 'utf8');
+const fullData = data1.concat('\n', data2, '\n');
+fs.writeFile(args[4], fullData, err => {
+  if (err) {
+    console.error(err);
+  }
+});
