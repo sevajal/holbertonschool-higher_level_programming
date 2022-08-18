1d7ba9bfb56098b209827a48794e7af23ecd695c
sevajal <sevajal@yahoo.com>
2022-08-18 15:18:17 -0500

    I simplified the code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,14 @@
 const axios = require('axios').default;
 let count = 0;
 axios.get(process.argv[2])
-  .then(function (response) {
-    for (let i = 0; i < response.data.results.length; i++) {
-      const obj = response.data.results[i].characters;
-      obj.forEach((link) => {
-        if (link.search('18') !== -1) {
+  .then((response) => {
+    const films = response.data.results;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.includes('18')) {
           count++;
         }
-      });
+      }
     }
     console.log(count);
   })
