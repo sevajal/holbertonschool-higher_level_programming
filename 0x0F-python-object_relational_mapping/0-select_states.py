600d4bfcb1e5121ee39807583020d1f1b1a6b47a
sevajal <sevajal@yahoo.com>
2022-08-30 08:55:47 -0500

    Documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 """script that lists all states from the database hbtn_0e_0_usa"""
+
+
 import MySQLdb
 import sys
 conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
