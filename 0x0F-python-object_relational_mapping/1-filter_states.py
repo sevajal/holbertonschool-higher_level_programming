04b0b8dfd47a72795ddb0a96a5592326fee843d1
sevajal <sevajal@yahoo.com>
2022-08-30 08:57:14 -0500

    Documentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 """script that lists all states from the database hbtn_0e_0_usa
 starting with N"""
+
+
 import MySQLdb
 import sys
 conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
