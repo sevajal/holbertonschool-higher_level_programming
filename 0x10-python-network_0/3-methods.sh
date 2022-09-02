86ca2f624d3d5c3207774aa740f41d8378c3c2e2
sevajal <sevajal@yahoo.com>
2022-09-02 11:29:00 -0500

    I changed the column to begin

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI $1 | grep -i Allow | awk '{for(i=1;i<=NF;i++) printf $i" "; print ""}'
+curl -sI $1 | grep -i Allow | awk '{for(i=2;i<=NF;i++) printf $i" "; print ""}'
