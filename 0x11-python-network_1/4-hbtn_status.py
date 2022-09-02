2d018898391db9a3fbb6eaca22eaea6ca4ca59f8
sevajal <sevajal@yahoo.com>
2022-09-01 22:07:04 -0500

    I added the name=main

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,9 @@
 '''script that fetches https://intranet.hbtn.io/status'''
 import requests
 
-r = requests.get('https://intranet.hbtn.io/status')
-r = r.text
-print("Body response:")
-print(f"\t- type: {type(r)}")
-print(f"\t- content: {r}")
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    r = r.text
+    print("Body response:")
+    print(f"\t- type: {type(r)}")
+    print(f"\t- content: {r}")
