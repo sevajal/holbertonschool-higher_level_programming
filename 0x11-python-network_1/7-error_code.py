ee98dfc63a8f5384aad35392b6fd14137ab3027e
sevajal <sevajal@yahoo.com>
2022-09-02 09:49:33 -0500

    I changed the print

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     code = r.status_code
     if code >= 400:
-        print(f'Error code: {code}')
+        print('Error code: {}'.format(code))
     else:
         print(r.text)
