901fe9f340ee2cade194c164986abd8b851879bf
sevajal <sevajal@yahoo.com>
2022-09-02 11:31:10 -0500

    I changed the print

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI $1 | grep -i Allow | awk '{for(i=2;i<=NF;i++) printf $i" "; print ""}'
+curl -sI $1 | grep -i Allow | awk '{print $2" "$3" "$4}'
