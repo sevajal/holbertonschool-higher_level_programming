0bf8baef2bfac30f8d59476901dbbbd85fb42859
sevajal <sevajal@yahoo.com>
2022-09-02 12:23:17 -0500

    I added a try

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,5 +5,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.headers['X-Request-Id'])
+    except:
+        print("None")
